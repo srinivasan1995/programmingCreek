08bc16090922c8e52ef04d78a0dd48ccfbf1e374
Srinivasan <r.srinivasan1995@gmail.com>
2019-06-15 21:47:36 +0530

    Create kthLargestElementInArray.cpp

---
+++ arraysAndStrings/kthLargestElementInArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n]={0};
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+
+	int k;
+	cin>>k;
+
+	sort(a,a+n);
+
+	for(int i=n-1;i>=0;i--){
+		if((k-1)==0){
+			cout<<a[i]<<endl;
+		}
+		k--;
+	}
+}
